6ad6c4838e9cf6de53752ecf032d0eb81e6be354
Tyler Morgan-Wall <tylermw@gmail.com>
2016-05-25 13:23:31 -0400

    Changed Monte Carlo method to work with RunMatrix as input instead of model matrix, no longer assigns type and levels attribute.
    Added checking for model matrix functionality from eval_design to eval_design_mc.

--- R/reducemodelmatrix.R
+++ R/reducemodelmatrix.R
@@ -5,36 +5,37 @@
 #'@param ModelMatrix The model matrix
 #'@param model The formula
 #'@return The reduced model matrix
+#'@export
 #'@examples mean(1)
-#'@keywords internal
+#'@keywords blah
 
-reducemodelmatrix = function(ModelMatrix,model) {
+reducemodelmatrix = function(RunMatrix,model) {
+  ModelMatrix = attr(RunMatrix,"modelmatrix")
+  ReduceRM = RunMatrix
   #save the attributes to reapply them to the reduced matrix
   attributelist = attributes(ModelMatrix)
   attributelist$dim = NULL
   attributelist$dimnames = NULL
 
   removecols = rep(TRUE,ncol(ModelMatrix))
-  for(parameter in colnames(attr(ModelMatrix,"Design"))) {
+  for(parameter in colnames(RunMatrix)) {
     if(!(parameter %in%  attr(terms(model),"term.labels"))) {
-      if(!is.null(attr(ModelMatrix,"type")[parameter]) && attr(ModelMatrix,"type")[parameter] == "factor") {
+      if(!is.null(sapply(RunMatrix,class)[parameter]) && sapply(RunMatrix,class)[parameter] == "factor") {
         removecols = (removecols & !(parameter == substr(colnames(ModelMatrix), 1, nchar(colnames(ModelMatrix))-1)))
         attributelist[["contrasts"]][[parameter]] = NULL
       }
-      if(!is.null(attr(ModelMatrix,"type")[parameter]) && attr(ModelMatrix,"type")[parameter] == "numeric") {
+      if(!is.null(sapply(RunMatrix,class)[parameter]) && sapply(RunMatrix,class)[parameter] == "numeric") {
         removecols = (removecols & !(parameter == colnames(ModelMatrix)))
       }
-      attributelist[["levels"]] = attributelist[["levels"]][!(parameter == names(attributelist[["levels"]]))]
-      attributelist[["type"]] = attributelist[["type"]][!(parameter == names(attributelist[["type"]]))]
-      if(!is.null(attributelist[["Design"]])) {
-        attributelist[["Design"]][parameter] = NULL
-      }
+      #attributelist[["levels"]] = attributelist[["levels"]][!(parameter == names(attributelist[["levels"]]))]
+      #attributelist[["type"]] = attributelist[["type"]][!(parameter == names(attributelist[["type"]]))]
+      ReduceRM[parameter] = NULL
     }
   }
   #return original attributes to new reduced model matrix
-  ReduceMM = ModelMatrix[,removecols]
+  attr(ReduceRM,"modelmatrix") = ModelMatrix[,removecols]
   for(i in 1:length(attributelist)) {
-    attr(ReduceMM, names(attributelist)[i]) = attributelist[[i]]
+    attr(attr(ReduceRM,"modelmatrix"), names(attributelist)[i]) = attributelist[[i]]
   }
-  return(ReduceMM)
+  return(ReduceRM)
 }

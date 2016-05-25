6d48b739608b582eec41219e3e8635b8da1ddadc
unknown <tmorganw@TMORGANW-LT.ida.org>
2016-05-25 14:37:03 -0400

    Removed gen_conservative_anticoef and moved functionality into gen_anticoef.

--- R/gen_anticoef.R
+++ R/gen_anticoef.R
@@ -1,8 +1,9 @@
-#'@title Generates default anticipated coefficients
+#'@title Generates Anticipated Coefficients
 #'
-#'@description Generates Default Anticipated Coefficients
+#'@description Generates Anticipated Coefficients
 #'
-#'@param modelmatrix The model matrix
+#'@param RunMatrix The run matrix
+#'@param conservative Default FALSE. If true, will generate conservative anticipated coefficients.
 #'@return Anticipated coefficients.
 #'@export
 #'@examples
@@ -10,22 +11,35 @@
 #'                   d=c(-1,1), stringsAsFactors = TRUE)
 #'de = gen_design(test,~(a+b+c+d),trials=12,optimality = "D",repeats=100)
 #'eval_design(de,0.05,gen_anticoef(de))
-gen_anticoef = function(RunMatrix) {
+#'eval_design(de,0.05,gen_anticoef(de),conservative=TRUE)
+gen_anticoef = function(RunMatrix,conservative=FALSE) {
 
   levels = sapply(sapply(RunMatrix,unique),length)-1
   type = sapply(RunMatrix,class)
 
   anticoef = c(1)
-  for(i in 1:length(levels)) {
-    if (type[i] == "factor" && levels[i] %% 2 == 0) {
-      anticoef = c(anticoef,rep(c(1,-1),levels[i]/2))
+  if(!conservative) {
+    for(i in 1:length(levels)) {
+      if (type[i] == "factor" && levels[i] %% 2 == 0) {
+        anticoef = c(anticoef,rep(c(1,-1),levels[i]/2))
+      }
+      if (type[i] == "factor" && levels[i] %% 2 == 1) {
+        anticoef = c(anticoef,1,rep(c(-1,1),(levels[i]-1)/2))
+      }
+      if (type[i] == "numeric") {
+        anticoef = c(anticoef,1)
+      }
     }
-    if (type[i] == "factor" && levels[i] %% 2 == 1) {
-      anticoef = c(anticoef,1,rep(c(-1,1),(levels[i]-1)/2))
+    return(anticoef)
+  } else {
+    for(i in 1:length(levels)) {
+      if (type[i] == "factor") {
+        anticoef = c(anticoef,1,rep(0,levels[i]-1))
+      }
+      if (type[i] == "numeric") {
+        anticoef = c(anticoef,1)
+      }
     }
-    if (type[i] == "numeric") {
-      anticoef = c(anticoef,1)
-    }
+    return(anticoef)
   }
-  return(anticoef)
 }

9060bb2abfb6de4bb2ea74a6f205811732d162a8
tylermorganwall <tylermw@gmail.com>
2016-07-25 17:40:34 -0400

    Version 0.7.1.0
    Finished writing RcppArmadillo-based A and I optimal design generation.

--- R/gen_momentsmatrix.R
+++ R/gen_momentsmatrix.R
@@ -14,20 +14,21 @@ gen_momentsmatrix = function(modelfactors) {
   linearterms = modelfactors[islinear]
   ordermatrix = matrix(0,nrow=length(linearterms),ncol = length(modelfactors))
   momentsmatrix = matrix(list(2),nrow=length(modelfactors),ncol=length(modelfactors))
+  momentsmatrixresults = matrix(0,nrow=length(modelfactors),ncol=length(modelfactors))
 
   for(i in 1:length(linearterms)) {
     ordermatrix[i,strsplit(modelfactors, split=linearterms[i],fixed=TRUE) != modelfactors] = 1
   }
   for(i in 1:length(linearterms)) {
-    if(any(lapply(strsplit(modelfactors, split=paste(linearterms[i],"^",sep=""),fixed=TRUE),length) > 1)) {
-      for(j in 1:ncol(ordermatrix)) {
+    if(any(lapply(strsplit(modelfactors[-1], split=paste(linearterms[i],"^",sep=""),fixed=TRUE),length) > 1)) {
+      for(j in 2:ncol(ordermatrix)) {
         if(length(strsplit(modelfactors[j], split=paste(linearterms[i],"^",sep=""),fixed=TRUE)[[1]]) > 1) {
           ordermatrix[i,j] = as.numeric(gsub("\\D","",strsplit(modelfactors[j],split="^",fixed=TRUE)[[1]][2]))
         }
       }
     }
   }
-  print(ordermatrix)
+  ordermatrix[,1] = 0
   for(i in 1:length(modelfactors)) {
     for(j in 1:length(modelfactors)) {
       momentsmatrix[i,j] = list(ordermatrix[,i]+ordermatrix[,j])
@@ -35,9 +36,9 @@ gen_momentsmatrix = function(modelfactors) {
       if(any((ordermatrix[,i]+ordermatrix[,j]) %% 2 == 1)) {
         momentsmatrix[i,j] = 0
       } else {
-        momentsmatrix[i,j] = prod(1/(momentsmatrix[i,j][[1]]+1))
+        momentsmatrixresults[i,j] = prod(1/(momentsmatrix[i,j][[1]]+1))
       }
     }
   }
-  return(momentsmatrix)
+  return(momentsmatrixresults)
 }

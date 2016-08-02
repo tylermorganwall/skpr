6621e342181ba404337be789be2d9fbe4d24f547
tylermorganwall <tylermw@gmail.com>
2016-08-02 15:20:25 -0400

    Version 0.8.4.0: Fixed implementation of modified Fedorov's algorithm, added proper row and column names to correlation matrix

--- src/genOptimalDesign.cpp
+++ src/genOptimalDesign.cpp
@@ -7,6 +7,10 @@ double delta(arma::mat V, arma::mat x, arma::mat y) {
   return(as_scalar(-x*V*x.t() + y*V*y.t() + (y*V*x.t())*(y*V*x.t()) - (x*V*x.t())*(y*V*y.t())));
 }
 
+double calculateDOptimality(arma::mat currentDesign) {
+  return(arma::det(currentDesign.t()*currentDesign));
+}
+
 double calculateIOptimality(arma::mat currentDesign, const arma::mat momentsMatrix) {
   double variance = trace(inv_sympd(currentDesign.t()*currentDesign)*momentsMatrix);
   return(variance);
@@ -23,7 +27,7 @@ double calculateAOptimality(arma::mat currentDesign) {
 //`@return stufff
 // [[Rcpp::export]]
 List genOptimalDesign(arma::mat initialdesign, const arma::mat candidatelist,const std::string condition,
-                      const arma::mat momentsmatrix, const NumericVector initialRows) {
+                      const arma::mat momentsmatrix, NumericVector initialRows) {
   int check = 0;
   int nTrials = initialdesign.n_rows;
   int maxSingularityChecks = nTrials;
@@ -60,92 +64,116 @@ List genOptimalDesign(arma::mat initialdesign, const arma::mat candidatelist,con
   double del = 0;
   int entryx = 0;
   int entryy = 0;
+  double newOptimum = 0;
+  double priorOptimum = 0;
+  double minDelta = 10e-5;
   arma::mat V;
   double newdel;
   //Generate a D-optimal design
   if(condition == "D") {
-    for (int i = 0; i < nTrials; i++) {
-      found = FALSE;
-      del = 0;
-      entryx = 0;
-      entryy = 0;
-      V = inv_sympd(initialdesign.t() * initialdesign);
-      for (int j = 0; j < totalPoints; j++) {
-        newdel = delta(V,initialdesign.row(i),candidatelist.row(j));
-        if(newdel > del) {
-          found = TRUE;
-          entryx = i; entryy = j;
-          del = newdel;
+    newOptimum = calculateDOptimality(initialdesign);
+    priorOptimum = newOptimum/2;
+    while((newOptimum - priorOptimum)/priorOptimum > minDelta) {
+      priorOptimum = newOptimum;
+      for (int i = 0; i < nTrials; i++) {
+        found = FALSE;
+        del = 0;
+        entryx = 0;
+        entryy = 0;
+        V = inv_sympd(initialdesign.t() * initialdesign);
+        for (int j = 0; j < totalPoints; j++) {
+          newdel = delta(V,initialdesign.row(i),candidatelist.row(j));
+          if(newdel > del) {
+            found = TRUE;
+            entryx = i; entryy = j;
+            del = newdel;
+          }
+        }
+        if (found) {
+          initialdesign.row(entryx) = candidatelist.row(entryy);
+          candidateRow[i] = entryy+1;
+          initialRows[i] = entryy+1;
+        } else {
+          candidateRow[i] = initialRows[i];
         }
       }
-      if (found) {
-        initialdesign.row(entryx) = candidatelist.row(entryy);
-        candidateRow[i] = entryy+1;
-      } else {
-        candidateRow[i] = initialRows[i];
-      }
+      newOptimum = calculateDOptimality(initialdesign);
     }
   }
   //Generate an I-optimal design
   if(condition == "I") {
     arma::mat temp;
-    double del = calculateIOptimality(initialdesign,momentsmatrix);
-    for (int i = 0; i < nTrials; i++) {
-      found = FALSE;
-      entryx = 0;
-      entryy = 0;
-      temp = initialdesign;
-      for (int j = 0; j < totalPoints; j++) {
-        //Checks for singularity; If singular, moves to next candidate in the candidate set
-        try {
-          temp.row(i) = candidatelist.row(j);
-          double newdel = calculateIOptimality(temp,momentsmatrix);
-          if(newdel < del) {
-            found = TRUE;
-            entryx = i; entryy = j;
-            del = newdel;
+    del = calculateIOptimality(initialdesign,momentsmatrix);
+    newOptimum = del;
+    priorOptimum = del/2;
+    while((newOptimum - priorOptimum)/priorOptimum > minDelta) {
+      priorOptimum = newOptimum;
+      for (int i = 0; i < nTrials; i++) {
+        found = FALSE;
+        entryx = 0;
+        entryy = 0;
+        temp = initialdesign;
+        for (int j = 0; j < totalPoints; j++) {
+          //Checks for singularity; If singular, moves to next candidate in the candidate set
+          try {
+            temp.row(i) = candidatelist.row(j);
+            newdel = calculateIOptimality(temp,momentsmatrix);
+            if(newdel < del) {
+              found = TRUE;
+              entryx = i; entryy = j;
+              del = newdel;
+            }
+          } catch (std::runtime_error& e) {
+            continue;
           }
-        } catch (std::runtime_error& e) {
-          continue;
+        }
+        if (found) {
+          initialdesign.row(entryx) = candidatelist.row(entryy);
+          candidateRow[i] = entryy+1;
+          initialRows[i] = entryy+1;
+        } else {
+          candidateRow[i] = initialRows[i];
         }
       }
-      if (found) {
-        initialdesign.row(entryx) = candidatelist.row(entryy);
-        candidateRow[i] = entryy+1;
-      } else {
-        candidateRow[i] = initialRows[i];
-      }
+      newOptimum = calculateIOptimality(initialdesign,momentsmatrix);
     }
   }
   //Generate an A-optimal design
   if(condition == "A") {
     arma::mat temp;
-    double del = calculateAOptimality(initialdesign);
-    for (int i = 0; i < nTrials; i++) {
-      found = FALSE;
-      entryx = 0;
-      entryy = 0;
-      temp = initialdesign;
-      for (int j = 0; j < totalPoints; j++) {
-        //Checks for singularity; If singular, moves to next candidate in the candidate set
-        try {
-          temp.row(i) = candidatelist.row(j);
-          double newdel = calculateAOptimality(temp);
-          if(newdel < del) {
-            found = TRUE;
-            entryx = i; entryy = j;
-            del = newdel;
+    del = calculateAOptimality(initialdesign);
+    newOptimum = del;
+    priorOptimum = del/2;
+    while((newOptimum - priorOptimum)/priorOptimum > minDelta) {
+      priorOptimum = newOptimum;
+      for (int i = 0; i < nTrials; i++) {
+        found = FALSE;
+        entryx = 0;
+        entryy = 0;
+        temp = initialdesign;
+        for (int j = 0; j < totalPoints; j++) {
+          //Checks for singularity; If singular, moves to next candidate in the candidate set
+          try {
+            temp.row(i) = candidatelist.row(j);
+            newdel = calculateAOptimality(temp);
+            if(newdel < del) {
+              found = TRUE;
+              entryx = i; entryy = j;
+              del = newdel;
+            }
+          } catch (std::runtime_error& e) {
+            continue;
           }
-        } catch (std::runtime_error& e) {
-          continue;
+        }
+        if (found) {
+          initialdesign.row(entryx) = candidatelist.row(entryy);
+          candidateRow[i] = entryy+1;
+          initialRows[i] = entryy+1;
+        } else {
+          candidateRow[i] = initialRows[i];
         }
       }
-      if (found) {
-        initialdesign.row(entryx) = candidatelist.row(entryy);
-        candidateRow[i] = entryy+1;
-      } else {
-        candidateRow[i] = initialRows[i];
-      }
+      newOptimum = calculateAOptimality(initialdesign);
     }
   }
   //return the model matrix and a list of the candidate list indices used to construct the run matrix

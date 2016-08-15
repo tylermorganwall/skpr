dca51e637e261978de895e9be2c3f9660acf9ea6
GeorgeMKhoury <george.m.khoury@gmail.com>
2016-08-15 18:26:05 -0400

    Update to work with lmer and glmer

--- R/effectSignificance.R
+++ R/effectSignificance.R
@@ -4,25 +4,35 @@
 #'
 #' @description Given a fit object and a specified type-1 error, return a vector of effect significances
 #'
-#' @param fit Fit object (e.g. from lm, glm, etc.).
+#' @param fit Fit object (e.g. from lm, glm, lmer, or glmer).
 #' @param alpha Type-1 error. If the p-value for an effect is less than alpha, it will be declared significant.
-#'
+#' @param parameter_assignment A vector indicating to which effect a parameter corresponds. Typically you would
+#' generate this from fit$assign [for an lm object], or from attr(model.matrix(fit), 'assign').
 #' @return Returns a vector of 1s and 0s, with 1 at position i indicating that
-#' effect i has at least one significant parameter.
+#' effect i is significant. The p-value for this test is computed with an F-test on the coeffecient
+#' estimates for that effect.
 #' The parameter names are given by c("(Intercept)", attr(terms(fit), 'term.labels')).
-#' The returned vector has a length max(fit$assign) + 1.
-#' p-values are computed with an F-test on the coeffecient estimates.
+#' The returned vector has a length max(parameter_assignment) + 1.
 #' @export
 #'
 effectSignificance <- function(fit, alpha, parameter_assignment) {
-  estimates = coef(fit)
-  variance = vcov(fit)
   retval = rep(0, max(parameter_assignment) + 1)
+
+  if (class(fit)[1] == "lm" || class(fit)[1] == "glm") {
+    estimates = coef(fit)
+    variance = vcov(fit)
+    dfres = df.residual(fit)
+  }
+  if (class(fit)[1] == "lmerMod" || class(fit)[1] == "glmerMod") {
+    estimates = coef(summary(fit))[, 1]
+    variance = as.matrix(vcov(fit))
+    dfres = df.residual(fit)
+  }
   for (i in 1:length(retval)) {
     L = parameter_assignment == (i - 1)  #picks out the rows/columns corresponding to the i-th effect
     g = sum(L)  #the degrees of freedom of the effect
     statistic = estimates[L] %*% solve(variance[L, L], estimates[L])
-    if (pf(statistic, g, fit$df.residual, lower.tail = FALSE) < alpha) {
+    if (pf(statistic, g, dfres, lower.tail = FALSE) < alpha) {
       retval[i] = 1
     }
   }
--- tests/testthat/testEffectSignificance.R
+++ tests/testthat/testEffectSignificance.R
@@ -5,9 +5,10 @@ context("effectSignificance")
 test_that("effectSignificance works with all continuous, lm", {
   #all continuous variables
   X = expand.grid(x1 = c(-1, 1), x2 = c(-1, 1))
+  set.seed(1)
   X$y = X$x1 + rnorm(nrow(X), 0, 0.00001)
   fit = lm(y ~ x1 + x2, data=X)
-  effect_sig = effectSignificance(fit, 0.05)
+  effect_sig = effectSignificance(fit, 0.05, fit$assign)
   expected = c(0, 1, 0)
   expected == effect_sig
   expect_equal(expected, effect_sig)
@@ -18,7 +19,7 @@ test_that("works with two categorical factors, one is significant, lm", {
   X$y = rep(1:8, length.out = nrow(X))
   fit = lm(y ~ x1 + x2, X)
   expected_effect_sig = c(0, 0, 1)
-  effect_sig = effectSignificance(fit, 0.05)
+  effect_sig = effectSignificance(fit, 0.05, fit$assign)
   effect_sig == expected_effect_sig
   expect_equal(expected_effect_sig, effect_sig)
 
@@ -29,48 +30,61 @@ test_that("works with continuous and categorical factors, lm", {
   X = expand.grid(x1 = c(-1,1), x2 = factor(c(1, 2, 3, 4)), x3 = factor(c('a', 'b', 'c')))
   X$y = rnorm(nrow(X), 0, 0.01)
   X$y[X$x2 == 2] = 1
+
   fit = lm(y ~ x1 + x2 + x3, X)
   expected_effect_sig = c(0, 0, 1, 0)
-  effect_sig = effectSignificance(fit, 0.05)
+  effect_sig = effectSignificance(fit, 0.05, fit$assign)
   effect_sig == expected_effect_sig
   expect_equal(effect_sig, expected_effect_sig)
 })
 
-#todo
+
 test_that("Works with glm", {
-  set.seed(1)
   X = expand.grid(x1 = c(-1,1), x2 = factor(c(1, 2, 3, 4)), x3 = factor(c('a', 'b', 'c')))
-  X$y = 0
-  X$y[X$x2 == 2] = 1
+  X = rbind(X, X, X, X)
+  set.seed(1999)
+  X$p = 1 / (1 + exp(-(X$x2 == 2)))
+  X$y = rbinom(nrow(X), 1, X$p)
+
   fit = glm(y ~ x1 + x2 + x3, data = X, family = 'binomial')
-  expected_effect_sig = c(0, 1, 1, 1)
-  effect_sig = effectSignificance(fit, 0.05)
+  summary(fit)
+  assignment = attr(model.matrix(fit), 'assign')
+  expected_effect_sig = c(0, 0, 0, 1)
+  effect_sig = effectSignificance(fit, 0.05, assignment)
   effect_sig == expected_effect_sig
   expect_equal(effect_sig, expected_effect_sig)
 })
 
-#todo
 test_that("Works with lmer", {
   set.seed(1)
   X = expand.grid(x1 = c(-1,1), x2 = factor(c(1, 2, 3, 4)), x3 = factor(c('a', 'b', 'c')))
   X$y = rnorm(nrow(X), 0, 0.01)
+  X['block1'] = rep(c(1, 2), each = nrow(X)/2)
   X$y[X$x2 == 2] = 1
-  fit = lm(y ~ x1 + x2 + x3, X)
-  expected_effect_sig = c(0, 0, 1, 0)
-  effect_sig = effectSignificance(fit, 0.05)
+  X$y = X$y + X$block1/10
+
+  fit = lme4::lmer(y ~ x1 + x2 + x3 + (1 | block1), X)
+  parameter_assignment = attr(model.matrix(fit), 'assign')
+  expected_effect_sig = c(1, 0, 1, 0)
+  effect_sig = effectSignificance(fit, 0.05, parameter_assignment)
   effect_sig == expected_effect_sig
   expect_equal(effect_sig, expected_effect_sig)
 })
 
 #todo
+
 test_that("Works with glmer", {
-  set.seed(1)
   X = expand.grid(x1 = c(-1,1), x2 = factor(c(1, 2, 3, 4)), x3 = factor(c('a', 'b', 'c')))
-  X$y = rnorm(nrow(X), 0, 0.01)
-  X$y[X$x2 == 2] = 1
-  fit = lm(y ~ x1 + x2 + x3, X)
-  expected_effect_sig = c(0, 0, 1, 0)
-  effect_sig = effectSignificance(fit, 0.05)
+  X = rbind(X, X, X, X)
+  X['block1'] = rep(c(1, 2), each = nrow(X)/2)
+  X$p = 1 / (1 + exp(-(X$x2 == 2) + X$block1/100))
+  set.seed(1999)
+  X$y = rbinom(nrow(X), 1, X$p)
+
+  fit = lme4::glmer(y ~ x1 + x2 + x3 + (1 | block1), X, family = 'binomial')
+  parameter_assignment = attr(model.matrix(fit), 'assign')
+  expected_effect_sig = c(0, 0, 0, 1)
+  effect_sig = effectSignificance(fit, 0.1, parameter_assignment)
   effect_sig == expected_effect_sig
   expect_equal(effect_sig, expected_effect_sig)
 })

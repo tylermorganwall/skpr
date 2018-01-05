1baaec9c1ad2cb8987898f23d3d6f167fc28f327
tylermorganwall <tylermw@gmail.com>
2018-01-05 14:33:23 -0500

    Version 0.45.0: Changed optimal search algorithm to return design with best aliasing structure (as determined by the alias matrix) when there is a tie between optimal designs. Increases likelyhood of producing an orthogonal design.
    Fixed bug with missing contrast list with hard-to-change factors. Fixed bug where factors were converted to integers with split-plot designs.
    
    Replaced highlighting regex with simpler version
    
    Added kableExtra to standalone shiny app

--- R/skprGUI.R
+++ R/skprGUI.R
@@ -1839,7 +1839,7 @@ skprGUI = function(inputValue1,inputValue2) {
           if(ncol(runmatrix()) > 1) {
             runmatrixprocessed = lapply(runmatrix(),spec_color_if)
             prelimhtml = kable_styling(knitr::kable(as.data.frame(runmatrixprocessed)[do.call(order,runmatrix()),],"html",row.names = TRUE,escape = F, align = "r"),"striped", full_width = F,position = "left")
-            gsub("(text-align:right;)(.+)(?=background-color: rgba\\(.+\\);)(background-color: rgba\\(.+\\);)",replacement = "\\1 \\3 \\2",x=prelimhtml,perl=TRUE)
+            gsub("(text-align:right;)(.+)(background-color: rgba\\(.+\\);)",replacement = "\\1 \\3 \\2",x=prelimhtml,perl=TRUE)
           } else {
             rownumbers = order(runmatrix()[,1])
             runreturn = list(runmatrix()[order(runmatrix()[,1]),])
@@ -1848,12 +1848,12 @@ skprGUI = function(inputValue1,inputValue2) {
             runmatrixprocessed = as.data.frame(lapply(runmatrixprocessed,spec_color_if))
             rownames(runmatrixprocessed) = rownumbers
             prelimhtml = kable_styling(knitr::kable(runmatrixprocessed,"html",row.names = TRUE,escape = F, align = "r"),"striped", full_width = F,position = "left")
-            gsub("(text-align:right;)(.+)(?=background-color: rgba\\(.+\\);)(background-color: rgba\\(.+\\);)",replacement = "\\1 \\3 \\2",x=prelimhtml,perl=TRUE)
+            gsub("(text-align:right;)(.+)(background-color: rgba\\(.+\\);)",replacement = "\\1 \\3 \\2",x=prelimhtml,perl=TRUE)
           }
         } else {
           runmatrixprocessed = lapply(runmatrix(),spec_color_if)
           prelimhtml = kable_styling(knitr::kable(as.data.frame(runmatrixprocessed),"html",row.names = TRUE,escape = F, align = "r"),"striped", full_width = F,position = "left")
-          gsub("(text-align:right;)(.+)(?=background-color: rgba\\(.+\\);)(background-color: rgba\\(.+\\);)",replacement = "\\1 \\3 \\2",x=prelimhtml,perl=TRUE)
+          gsub("(text-align:right;)(.+)(background-color: rgba\\(.+\\);)",replacement = "\\1 \\3 \\2",x=prelimhtml,perl=TRUE)
         }
       } else {
         if(input$orderdesign) {

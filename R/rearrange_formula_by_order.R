bae3f3816f7e4a728efa5d0fea78f6d961da9bf7
tylermorganwall <tylermw@gmail.com>
2019-02-12 16:09:11 -0500

    Fix no intercept term when rearranging formula

--- R/rearrange_formula_by_order.R
+++ R/rearrange_formula_by_order.R
@@ -5,9 +5,14 @@
 #'@return Rearranged model
 #'@keywords internal
 rearrange_formula_by_order = function(model) {
+  interceptterm = attr(terms.formula(model),"intercept") == 1
   model_terms = attr(terms.formula(model),"term.labels")
   modelorder = attr(terms.formula(model),"order")
   higherorderterms = grepl("^I\\(.+\\)$",x=attr(terms.formula(model),"term.labels"),perl=TRUE)
   entries_move_end = (modelorder == 1 & higherorderterms)
-  formula(paste0(c("~",paste0(c(model_terms[!entries_move_end],model_terms[entries_move_end]),collapse = " + ")),collapse=""))
+  rearranged_model = formula(paste0(c("~",paste0(c(model_terms[!entries_move_end],model_terms[entries_move_end]),collapse = " + ")),collapse=""))
+  if(!interceptterm) {
+    rearranged_model = update.formula(rearranged_model,~ . + -1)
+  }
+  rearranged_model
 }

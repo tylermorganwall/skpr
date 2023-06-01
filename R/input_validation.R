f76268edf8519968a71d1c9eeddf59360381df93
Morgan-Wall <tmorganw@ida.org>
2023-06-01 18:01:27 -0400

    -Add input validation function

---
+++ R/input_validation.R
@@ -0,0 +1,9 @@
+#'@title Check Model Formula Validity
+#'
+#'@keywords internal
+check_model_validity = function(model) {
+  if(length(model) != 2) {
+    stop(sprintf("Model (`%s`) should not have a left-hand side (here, given as `%s`). Only include the right-hand side of the formula.",
+                 as.character(model), as.character(model[2])))
+  }
+}
